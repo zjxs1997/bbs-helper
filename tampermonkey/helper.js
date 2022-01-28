2df96e44e10e5192b8d6a55a85fca2a9d5c8d83b
zjxs1997 <1500012780@pku.edu.cn>
2022-01-28 10:35:25 +0800

    按钮还是没解决

--- tampermonkey/helper.js
+++ tampermonkey/helper.js
@@ -12,28 +12,30 @@
 
 // ==/UserScript==
 
-function is_thread_page(s){
+
+
+function is_thread_page(s) {
     return s.includes('bid') && s.includes('threadid') && s.includes('post-read.php');
 }
 
-function get_bid_threadid(s){
-    s=s.split('&page')[0];
-    s=s.split('?bid=')[1];
+function get_bid_threadid(s) {
+    s = s.split('&page')[0];
+    s = s.split('?bid=')[1];
     return s.split('&threadid=');
 }
 
-function get_current_time(){
-    var now=new Date();
-    const offset=now.getTimezoneOffset();
-    now=new Date(now.getTime()-(offset*60*1000));
+function get_current_time() {
+    var now = new Date();
+    const offset = now.getTimezoneOffset();
+    now = new Date(now.getTime() - (offset * 60 * 1000));
     return now.toISOString().replace('T', ' ').split('.')[0];
 }
 
-(function() {
+(function () {
     console.log("[bbs helper] start");
     var cssFix = document.createElement('style');
     // 左侧边栏隐私模块
-    if (1){
+    if (1) {
         // 隐藏头像，
         cssFix.innerHTML += '.portrait.pic{display: none}'
         // 隐藏id
@@ -45,7 +47,7 @@ function get_current_time(){
     }
 
     // 只读模块
-    if (1){
+    if (1) {
         // 隐藏发送按钮
         cssFix.innerHTML += '.publish-button.extended{ display: none!important;}';
         // 隐藏输入文本的框框
@@ -59,41 +61,49 @@ function get_current_time(){
 
     // 帖子收藏模块
     if (1) {
-        const ft_str='favorite_threads';
-        var favorite_threads=GM_getValue(ft_str, []);
+        const ft_str = 'favorite_threads';
+        var favorite_threads = GM_getValue(ft_str, []);
         console.log(favorite_threads);
         // favorite_threads中每个元素应该是[帖子标题，收藏时间，bid，threadid]
-    
-        var current_url=window.location.href;
-    
-        if (is_thread_page(current_url)){
-            var bid_threadid=get_bid_threadid(current_url);
-            var bid=bid_threadid[0], threadid=bid_threadid[1];
-            var favorite_flag=false;
-            for (var ft_index=0; ft_index<favorite_threads.length;++ft_index){
-                if (favorite_threads[ft_index][2]==bid && favorite_threads[ft_index][3]==threadid){
-                    favorite_flag=true;
+
+        var current_url = window.location.href;
+
+        if (is_thread_page(current_url)) {
+            var bid_threadid = get_bid_threadid(current_url);
+            var bid = bid_threadid[0], threadid = bid_threadid[1];
+            var favorite_flag = false;
+            for (var ft_index = 0; ft_index < favorite_threads.length; ++ft_index) {
+                if (favorite_threads[ft_index][2] == bid && favorite_threads[ft_index][3] == threadid) {
+                    favorite_flag = true;
                     break;
                 }
             }
-    
+
             // todo，创建收藏按钮等
             if (favorite_flag) {
-    
+
             } else {
-    
+
             }
         }
-    
-        if (current_url == 'https://bbs.pku.edu.cn/v2/favorite_thread') {
-            // todo
-            document.title="帖子收藏夹";
-            document.body.innerHTML='fuckyou';
 
+        if (current_url == 'https://bbs.pku.edu.cn/v2/favorite_thread') {
+            document.title = "帖子收藏夹";
+            document.head.innerHTML = '';
             document.body.innerHTML += '<a href="home.php"> 返回首页 </a>';
+            // table
+            document.body.innerHTML += '<table><tr><th>帖子标题</th><th>收藏时间</th><th>链接</th><th></th></tr>';
+            for (var i = 0; i < favorite_threads.length; ++i) {
+                document.body.innerHTML += '<td><tr>' + favorite_threads[i][0] + '</tr><tr>' + favorite_threads[i][1] + '</tr><tr>';
+                document.body.innerHTML += '<a href="https://bbs.pku.edu.cn/v2/post-read.php?bid=' + favorite_threads[i][2] + '&threadid=' + favorite_threads[i][3] + '">' + '</a></tr><tr>';
+                // todo 按钮
+                document.body.innerHTML += '</tr><td>';
+            }
+            document.body.innerHTML += '</table>';
+
         } else {
             // 添加一个按钮
-            var link_buttons=document.getElementById('link-buttons');
+            var link_buttons = document.getElementById('link-buttons');
             link_buttons.innerHTML += '<a href="favorite_thread", id="tab_favorite_thread"> 收藏的帖子 </a>';
         }
     }
